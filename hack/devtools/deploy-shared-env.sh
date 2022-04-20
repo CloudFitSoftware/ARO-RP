8c2e99df4e3ca0d0d38edbe4e53ab75f968cda62
Zach Jones <zachjones@cloudfitsoftware.com>
2022-04-20 12:50:09 -0600

    feat: local dev updates

--- hack/devtools/deploy-shared-env.sh
+++ hack/devtools/deploy-shared-env.sh
@@ -66,8 +66,7 @@ deploy_env_dev() {
             "proxyImage=arointsvc.azurecr.io/proxy:latest" \
             "proxyImageAuth=$(jq -r '.auths["arointsvc.azurecr.io"].auth' <<<$PULL_SECRET)" \
             "proxyKey=$(base64 -w0 <secrets/proxy.key)" \
-            "sshPublicKey=$(<secrets/proxy_id_rsa.pub)" \
-            "vpnCACertificate=$(base64 -w0 <secrets/vpn-ca.crt)" >/dev/null
+            "sshPublicKey=$(<secrets/proxy_id_rsa.pub)" >/dev/null
 }
 
 deploy_env_dev_override() {
